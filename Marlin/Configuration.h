fc34fdd9b039e2beead61fb6923a72b1952def01
quokecola <Qc2006678>
2020-07-22 01:43:27 +0800

    Add CLion Configuration.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -962,7 +962,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, -36, -1.1 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -36, -0.8 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
